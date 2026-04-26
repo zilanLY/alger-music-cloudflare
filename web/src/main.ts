9c9748989d62af01e9d0448b08a219a1af7e56ac
zilanLY <zilan@ioipmc.ggff.net>
2026-04-26 10:12:13 +0800

    add: web/src/main.ts

---
+++ web/src/main.ts
@@ -0,0 +1,25 @@
+﻿import './polyfills/electron';
+import './index.css';
+import '@/assets/css/mobile.css';
+import 'animate.css';
+import 'remixicon/fonts/remixicon.css';
+
+import { createApp } from 'vue';
+
+import i18n from '@/i18n/renderer';
+import router from '@/router';
+import pinia from '@/store';
+
+import App from './App.vue';
+import directives from './directive';
+
+const app = createApp(App);
+
+Object.keys(directives).forEach((key: string) => {
+  app.directive(key, directives[key as keyof typeof directives]);
+});
+
+app.use(pinia);
+app.use(router);
+app.use(i18n as any);
+app.mount('#app');
