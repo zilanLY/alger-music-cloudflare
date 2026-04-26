6e86fdd1e91c5c6cdae92c7938d66194243f1962
zilanLY <zilan@ioipmc.ggff.net>
2026-04-26 10:11:55 +0800

    add: web/src/i18n/renderer.ts

---
+++ web/src/i18n/renderer.ts
@@ -0,0 +1,18 @@
+﻿import { createI18n } from 'vue-i18n';
+
+import { DEFAULT_LANGUAGE, FALLBACK_LANGUAGE } from './languages';
+import { buildLanguageMessages } from './utils';
+
+// 使用工具函数构建语言消息对象
+const messages = buildLanguageMessages();
+
+const i18n = createI18n({
+  legacy: locale: DEFAULT_LANGUAGE,
+  fallbackLocale: FALLBACK_LANGUAGE,
+  messages,
+  globalInjection: true,
+  silentTranslationWarn: true,
+  silentFallbackWarn: true
+});
+
+export default i18n;
